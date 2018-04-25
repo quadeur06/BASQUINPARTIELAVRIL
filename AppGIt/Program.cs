bb16ec2b7c5125e74867d007ba5300ce410e5b06
unknown <ybasquin@siclade.fr>
2018-04-25 09:08:21 +0200

    premier commit

---
+++ AppGIt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
